dcf7338bc325e844ed41eedd71939681d0f06a35
Emiliano Maccaferri <inbox@emilianomaccaferri.com>
2020-01-21 17:04:02 +0100

    versione alternativa per il propagamento del fuoco
    non voglio fare l'intruso e lo spaccaballe, però così mi sembrava un pelino più chiara

--- esame-35/fire_sym/fire_sym.c
+++ esame-35/fire_sym/fire_sym.c
@@ -43,3 +43,50 @@ void propagate_fire(const struct forest *f)
 	free(foresta2);
 }
 
+// Versione alternativa (idk magari è un po' più chiara) e scusa l'intrusione
+void propagate_fire(const struct forest* f) {
+
+	int total = f->cols * f->rows;
+	char* newData = malloc(total);
+
+	strncpy(newData, f->data, total);
+	//newData[total] = 0;
+
+	for (int i = 0; i < f->rows; i++) {
+
+		for (int k = 0; k < f->cols; k++) {
+
+			int currentIndex = (i * f->cols) + k,
+				south = ((i + 1) * f->cols) + k,
+				north = ((i - 1) * f->cols) + k,
+				west = currentIndex - 1,
+				east = currentIndex + 1,
+				leftBorder = (i * f->cols),
+				rightBorder = (i * f->cols) + f->cols - 1,
+				northBorder = 0,
+				southBorder = total - 1;
+
+			// current to west
+			if (west >= leftBorder && newData[currentIndex] == 'F' && f->data[currentIndex] != '.')
+				newData[west] = 'F';
+
+			// current to east
+			if (east <= rightBorder && newData[currentIndex] == 'F' && f->data[currentIndex] != '.')
+				newData[east] = 'F';
+
+			// current to north
+			if (north >= northBorder && newData[currentIndex] == 'F' && f->data[currentIndex] != '.')
+				newData[north] = 'F';
+
+			// current to south
+			if (south <= southBorder && newData[currentIndex] == 'F' && f->data[currentIndex] != '.')
+				newData[south] = 'F';
+
+		}
+
+	}
+
+	strncpy(f->data, newData, total);
+	free(newData);
+
+}
